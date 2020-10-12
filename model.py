22e77daeccbc7ad01b960d2f346cfa616f35c397
Yilin Zhang <yzhang0129@outlook.com>
2020-10-11 22:20:55 -0400

    20201011222057

--- model.py
+++ model.py
@@ -7,6 +7,8 @@ from torch.utils.data import Dataset, DataLoader
 import pickle
 import numpy as np
 from tqdm import trange
+from pathlib import Path
+from datetime import datetime
 
 
 class MelDataset(Dataset):
@@ -30,7 +32,7 @@ class MelDataset(Dataset):
                                                      features.size(1),
                                                      features.size(2)))
 
-        print('features size:', self.features.size())
+        # print('features size:', self.features.size())
 
     def __getitem__(self, index):
         return self.features[index], self.labels[index]
@@ -87,36 +89,56 @@ class MyCNN(nn.Module):
     def __init__(self):
         super(MyCNN, self).__init__()
         self.conv1 = nn.Conv2d(1, 16, 3)  # dilation is not added here
+        self.bn1 = nn.BatchNorm2d(16)
         self.pool1 = nn.MaxPool2d(2, stride=2)
         self.drp = nn.Dropout2d(0.25)
         self.conv2 = nn.Conv2d(16, 32, 3)
+        self.bn2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(32, 64, 3)
+        self.bn3 = nn.BatchNorm2d(64)
         self.conv4 = nn.Conv2d(64, 128, 3)
+        self.bn4 = nn.BatchNorm2d(128)
         self.conv5 = nn.Conv2d(128, 64, 3)
+        self.bn5 = nn.BatchNorm2d(64)
         self.pool2 = nn.MaxPool2d(4, stride=4)
-        self.fc1 = nn.Linear(14*6, 32)
+        self.fc1 = nn.Linear(64*6*2, 64)
 
-        self.fc2_1 = nn.Linear(512, 3, bias=True)
-        self.fc2_2 = nn.Linear(512, 3, bias=True)
-        self.fc2_3 = nn.Linear(512, 3, bias=True)
-        self.fc2_4 = nn.Linear(512, 3, bias=True)
-        self.fc2_5 = nn.Linear(512, 3, bias=True)
-        self.fc2_6 = nn.Linear(512, 3, bias=True)
-        self.fc2_7 = nn.Linear(512, 3, bias=True)
-        self.fc2_8 = nn.Linear(512, 3, bias=True)
-        self.fc2_9 = nn.Linear(512, 3, bias=True)
-        self.fc2_10 = nn.Linear(512, 3, bias=True)
-        self.fc2_11 = nn.Linear(512, 3, bias=True)
+        self.fc2_1 = nn.Linear(64, 3, bias=True)
+        self.fc2_2 = nn.Linear(64, 3, bias=True)
+        self.fc2_3 = nn.Linear(64, 3, bias=True)
+        self.fc2_4 = nn.Linear(64, 3, bias=True)
+        self.fc2_5 = nn.Linear(64, 3, bias=True)
+        self.fc2_6 = nn.Linear(64, 3, bias=True)
+        self.fc2_7 = nn.Linear(64, 3, bias=True)
+        self.fc2_8 = nn.Linear(64, 3, bias=True)
+        self.fc2_9 = nn.Linear(64, 3, bias=True)
+        self.fc2_10 = nn.Linear(64, 3, bias=True)
+        self.fc2_11 = nn.Linear(64, 3, bias=True)
 
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x):
-        x = self.drp(F.relu(self.conv1(x)))
-        x = self.drp(F.relu(self.conv2(x)))
-        x = self.drp(self.pool1(F.relu(self.conv3(x))))
-        x = self.drp(F.relu(self.conv4(x)))
-        x = self.drp(self.pool2(F.relu(self.conv5(x))))
-        return x
+        x = self.drp(self.pool1(F.relu(self.bn1(self.conv1(x)))))
+        x = self.drp(F.relu(self.bn2(self.conv2(x))))
+        x = self.drp(self.pool1(F.relu(self.bn3(self.conv3(x)))))
+        x = self.drp(F.relu(self.bn4(self.conv4(x))))
+        x = self.drp(self.pool2(F.relu(self.bn5(self.conv5(x)))))
+        x = x.view(-1, 64*6*2)
+        x = self.fc1(x)
+        x1 = self.softmax(self.fc2_1(x))
+        x2 = self.softmax(self.fc2_2(x))
+        x3 = self.softmax(self.fc2_3(x))
+        x4 = self.softmax(self.fc2_4(x))
+        x5 = self.softmax(self.fc2_5(x))
+        x6 = self.softmax(self.fc2_6(x))
+        x7 = self.softmax(self.fc2_7(x))
+        x8 = self.softmax(self.fc2_8(x))
+        x9 = self.softmax(self.fc2_9(x))
+        x10 = self.softmax(self.fc2_10(x))
+        x11 = self.softmax(self.fc2_11(x))
+
+        return x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11
+        # return x
         # x = F.relu(self.lin1(x))
         # x = F.relu(self.lin2(x))
         # x = F.relu(self.lin3(x))
@@ -135,19 +157,18 @@ class MyCNN(nn.Module):
         # return x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11
 
 
-def train_dnn(device, train_set, val_set, num_epochs, batch_size):
+def train(device, train_set, val_set, num_epochs, batch_size, net, writer_path):
     train_loader = DataLoader(train_set, batch_size, shuffle=True)
     val_loader = DataLoader(val_set, len(val_set), shuffle=True)
 
-    net = MyDNN()
     net.to(device)
 
     optimizer = optim.Adam(net.parameters())
     criterion = nn.CrossEntropyLoss()
-    writer = SummaryWriter('runs/test1')
+    writer = SummaryWriter(writer_path)
 
-    for epoch in range(num_epochs):
-        print('epoch:', epoch, '...')
+    for epoch in trange(num_epochs):
+        # print('epoch:', epoch, '...')
         for i, (inputs, labels) in enumerate(train_loader):
             inputs = inputs.to(device)
             labels = labels.to(device)
@@ -166,9 +187,9 @@ def train_dnn(device, train_set, val_set, num_epochs, batch_size):
             writer.add_scalar('Loss/train', loss, epoch)
 
         # validate every 5 epochs
-        if (epoch != 0 and epoch % 5 == 0):
+        if ((epoch + 1) % 5 == 0):
             # val_inputs, val_labels = iter(val_loader).next()
-            accuracies = [[]] * 11
+            accuracies = [[] for i in range(11)]
             # NOTE: Since I set only one batch for validation, the for loop
             # is only for one iteration
             for val_inputs, val_labels in val_loader:
@@ -186,27 +207,39 @@ def train_dnn(device, train_set, val_set, num_epochs, batch_size):
 
             writer.add_scalar('Loss/val', val_loss, epoch)
             accuracies = torch.tensor(accuracies)
-            print(f'accuracy size: {accuracies.size()}')
+            # print(f'accuracy size: {accuracies.size()}')
             accuracies = torch.mean(accuracies, dim=1)
             for k in range(11):
-                print(f'accuracy {k}: {accuracies[k]}')
+                # print(f'accuracy {k}: {accuracies[k]}')
+                writer.add_scalar(f'Accuracy/feature_{k}', accuracies[k], epoch)
+                print(f'Accuracy/feature_{k}: {accuracies[k]}')
 
-        print('finished')
+        # print('finished')
 
     writer.flush()
     writer.close()
+
+    time_stamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    Path('models').mkdir(parents=True, exist_ok=True)
+    torch.save(cnn.state_dict(), f'models/{time_stamp}.pt')
 
 
 if __name__ == '__main__':
     torch.manual_seed(0)
     np.random.seed(0)
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    num_epochs = 50
-    batch_size = 50
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    num_epochs = 500
+    batch_size = 10
 
     lengths = [2795, 350, 349]
-    dataset = MelDataset(True)
-    train_set, val_set, test_set = torch.utils.data.random_split(dataset, lengths)
 
-    train_dnn(device, train_set, val_set, num_epochs, batch_size)
+    # dataset = MelDataset(True)
+    # train_set, val_set, test_set = torch.utils.data.random_split(dataset, lengths)
+    # dnn = MyDNN()
+    # train(device, train_set, val_set, num_epochs, batch_size, dnn, 'runs/dnn')
+
+    dataset = MelDataset(False)
+    train_set, val_set, test_set = torch.utils.data.random_split(dataset, lengths)
+    cnn = MyCNN()
+    train(device, train_set, val_set, num_epochs, batch_size, cnn, 'runs/cnn')
